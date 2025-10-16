663c7998d5bb3b8920dbd818ac55a1c9dcd3c199
GambitTi <85141014+GambitTi@users.noreply.github.com>
2025-10-16 07:53:02 -0700

    Update config.py

--- backend/config.py
+++ backend/config.py
@@ -7,4 +7,4 @@ class Config:
 
     JWT_SECRET_KEY = os.environ["JWT_SECRET_KEY"]
     ACCESS_TOKEN_EXPIRES = timedelta(minutes=int(os.getenv("ACCESS_TOKEN_MINUTES", "15")))
-    REFRESH_TOKEN_EXPIRES = timedelta(days=int(os.getenv("REFRESH_TOKEN_DAYS", "7")))
+    REFRESH_TOKEN_EXPIRES = timedelta(days=int(os.getenv("REFRESH_TOKEN_DAYS", "1")))            #Refresh rate for tokens
