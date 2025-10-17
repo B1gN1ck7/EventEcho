b78628451c510f26fddc4c74ad36bfc2b372b2c8
coopc04 <coopc04@gmail.com>
2025-10-17 10:50:41 -0400

    Remove database connection test code
    Remove test connection code that checks database time.

--- backend/test_connection.py
+++ backend/test_connection.py
@@ -1,10 +1,1 @@
-from database import create_connection
 
-conn = create_connection()
-
-if conn:
-    cur = conn.cursor()
-    cur.execute("SELECT NOW();")
-    print("Current time from database:", cur.fetchone())
-    cur.close()
-    conn.close()
